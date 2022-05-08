a14bed0a318c373f66e9de10b754d003df823fb4
Shruti <shrutiperiwal21@gmail.com>
2022-05-08 10:48:34 +0530

    Remainder

---
+++ Week1/Program3.cpp
@@ -0,0 +1,10 @@
+//PrepBuddy gave you a number x, and asked you to find the remainder of x divided by 10
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x, rem;
+	cin>>x;
+	rem = x%10;
+	cout<<rem;
+	return 0;
+}
