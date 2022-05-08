0de96c29522bec654052cef2a9c1e6ab1b4d17b6
Shruti <shrutiperiwal21@gmail.com>
2022-05-08 11:38:41 +0530

    PrepBuddy and Socks

---
+++ Week1/Program6.cpp
@@ -0,0 +1,15 @@
+//PrepBuddy and Socks - PrepBuddy is getting late for college. She is looking for a matching pair of socks from a bos full of socks. In the worst case scenario, how many socks should
+//Prepbuddy remove from the drawer until she finds a matching pair.
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	for(int i=1; i<=t; i++) {
+		int n;
+		cin>>n;
+		n = n+1;
+		cout<<n<<endl;
+	}
+	return 0;
+}
