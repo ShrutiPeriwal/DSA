c7a577e707c7c1666aff20ae302a75841510f94d
Shruti <shrutiperiwal21@gmail.com>
2022-05-08 10:39:19 +0530

    Input two Numbers

--- Week1/Program1.cpp
+++ Week1/Program1.cpp
@@ -1,1 +1,10 @@
 //Write a program to take two integers x and y as input from the user, add those two integers and print the output
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x, y, z;
+	cin>>x>>y;
+	z= x+y;
+	cout<<z;
+	return 0;
+}
