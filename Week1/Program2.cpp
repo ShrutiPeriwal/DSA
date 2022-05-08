43e1061ac3ba4d8627865b5d9f56ffeb6fe8dee3
Shruti <shrutiperiwal21@gmail.com>
2022-05-08 10:43:49 +0530

    Multiply two Numbers

---
+++ Week1/Program2.cpp
@@ -0,0 +1,10 @@
+//Write a Program to take two integers x and y as input from the user, multiply those integers and print the product
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x, y, z;
+	cin>>x>>y;
+	z = x*y;
+	cout<<z;
+	return 0;
+}
