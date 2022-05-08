13127ef7e74807ab0a86a32efad9d25d4304f565
Shruti <shrutiperiwal21@gmail.com>
2022-05-08 10:58:24 +0530

    Last Digit

---
+++ Week1/Program4.cpp
@@ -0,0 +1,10 @@
+//Let's solve a problem, which will help you solving many problems going ahead.Find last digit of a given numberx.
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x , ld;
+	cin>>x;
+	ld = x%10;
+	cout<<ld;
+	return 0;
+}
